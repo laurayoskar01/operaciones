d6d62f4a29c80a167f6c6984c53eacabd5b17136
laura <lauraranjel415@gmail.com>
2023-11-08 07:39:50 -0500

    configuracion de calculadora

--- github.js
+++ github.js
@@ -1,74 +1,29 @@
-function calculadora() {
+function seleccionarOperacion(operacion) {
+    var num1 = parseFloat(document.getElementById("numero1").value);
+    var num2 = parseFloat(document.getElementById("numero2").value);
+    var resultado = 0;
 
-    var num1 = document.getElementById("numero1").value;
-    var num2 = document.getElementById("numero2").value;
-    var operacion = 0;
-    var opcion = 0;
-    
-    numero1 = parseInt(numero1);
-    numero2 = parseInt(numero2);
-
-  
-    
-
-
-do {
-    opcion = parseInt(prompt(" ************** calculadora**************\n1.suma\n2.resta\n3.multiplicacion\n4.division\n5.salir "));
-    if (opcion < 5) {
-        num1 = parseInt(prompt(" digite un numero: "));
-
-        if (opcion == 4) {
-
-            do {
-
-                num2 = parseInt(prompt(" digite otro numero: "));
-
-                if (num2 == 0) {
-
-                    alert(" o es posible hacer division por 0 ");
-
-                }
-
-            } while (num2 == 0);
-
-        } else {
-
-            num2 = parseInt(prompt(" digite otro numero: "));
-        }
-
-
-
-
-    } else {
-        alert(" opcion invalida ");
-    }
-
-    switch (opcion) {
-
-        case 1: //suma
-            operacion = num1 + num2;
+    switch (operacion) {
+        case 'suma':
+            resultado = num1 + num2;
             break;
-
-        case 2: //resta
-            operacion = num1 - num2;
+        case 'resta':
+            resultado = num1 - num2;
             break;
-
-        case 3: //multiplicacion
-            operacion = num1 * num2;
+        case 'multiplicacion':
+            resultado = num1 * num2;
             break;
-
-        case 4: //division
-
-            operacion = num1 / num2;
+        case 'division':
+            if (num2 !== 0) {
+                resultado = num1 / num2;
+            } else {
+                alert("No es posible dividir por 0.");
+            }
             break;
-
         default:
+            alert("Operación no válida");
             break;
     }
-    if (opcion < 5) {
-        alert(" el resultado de esta operacion es: " + operacion);
-    }
 
-}
-while (opcion < 5 || opcion > 5);
+    document.getElementById("resultado").textContent = "Resultado: " + resultado;
 }
